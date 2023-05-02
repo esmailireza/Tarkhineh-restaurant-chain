ad7ca3a498a1e656c2903662c07eb80101fa8e9a
esmailireza <esmailireza20@gmail.com>
2023-05-02 23:42:05 +0430

    add fonts to project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Header from "./components/header/header";
 
 function App() {
   return (
-    <div className="App">
+    <div className={`App font-Estedad-VF`}>
       <Header />
     </div>
   );
