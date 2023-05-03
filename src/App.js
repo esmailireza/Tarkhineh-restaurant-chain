c3d07446db4bec8495ee87538fa86ff140cdba1d
reza <esmailireza20@gmail.com>
2023-05-03 17:45:21 +0330

    implement swiper component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/header/header";
+import SwiperComponent from "./components/swiper/swiper";
 
 function App() {
   return (
     <div className={`App font-Estedad-VF`}>
       <Header />
+      <SwiperComponent/>
     </div>
   );
 }
---
+++ src/components/swiper/swiper.js
@@ -0,0 +1,41 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./swiper.css";
+
+// import required modules
+import { Keyboard, Pagination, Navigation } from "swiper";
+
+
+const SwiperComponent = ()=>{
+    return(
+        <div className="mySwiper w-full h-full bg-red-500">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          keyboard={{
+            enabled: true,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Keyboard, Pagination, Navigation]}
+          className="mySwiper w-full h-full bg-red-500"
+        >
+          <SwiperSlide className="bg-red-500">Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+        </Swiper>
+        </div>
+    )
+}
+
+export default SwiperComponent;
